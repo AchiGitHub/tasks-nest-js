c56c1d8028a645bbf4ff81548cb8bcdca793569d
Achintha Ikiriwatte <achinthaikiriwatte@Achinthas-MacBook-Pro.local>
2020-11-09 22:07:28 +0530

    added validation on errors

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -28,6 +29,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto)
     }
@@ -40,7 +42,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTask(
         @Param('id') id: string,
-        @Body('status') status: TaskStatus
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus
     ) {
         return this.tasksService.updateTask(id, status);
     }
